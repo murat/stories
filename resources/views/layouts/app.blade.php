f7488c9a02abb6c376a8d24f9f1708fe460ae8ed
Murat Bastas <muratbsts@gmail.com>
2017-07-23 15:43:27 +0300

    Environment for assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/app.css', Config::get('app.env') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -75,6 +75,6 @@
     </div>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
+    <script src="{{ asset('js/app.js', Config::get('app.env')) }}"></script>
 </body>
 </html>
